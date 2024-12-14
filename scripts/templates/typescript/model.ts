5dda08161f912954cccdf6c71b482b55e7d83536
yna87 <yna.sakana@gmail.com>
2024-12-14 16:10:44 +0900

    fix model template

--- scripts/templates/typescript/model.ts
+++ scripts/templates/typescript/model.ts
@@ -1,10 +1,9 @@
 export class {{ table.name }} { 
-    {% for column in table.columns %}
-    {{ column.name | camelcase }}: {{ column.ts_type }};
-    {% endfor %}
-
     constructor(init?: Partial<{{ table.name }}>) {
         Object.assign(this, init);
     }
-}
 
+    {% for column in table.columns %}
+    {{ column.name | camelcase }}?: {{ column.ts_type }};
+    {% endfor %}
+}
